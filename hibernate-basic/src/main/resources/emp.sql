d84e0a01394090d859446edd9d3cef6b3eb9a03b
nnares <nishchay.naresh@gmail.com>
2020-12-01 00:56:20 +0530

    + com.nish.ems

---
+++ hibernate-basic/src/main/resources/emp.sql
@@ -0,0 +1,7 @@
+create table EMP (
+   id INT NOT NULL auto_increment,
+   first_name VARCHAR(20) default NULL,
+   last_name  VARCHAR(20) default NULL,
+   salary     INT  default NULL,
+   PRIMARY KEY (id)
+);
