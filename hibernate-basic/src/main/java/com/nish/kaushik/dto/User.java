69ea740f4ff31844b127ba4d41eb93af8267a14c
nnares <nishchay.naresh@gmail.com>
2020-12-23 01:56:23 +0530

    + com.nish.kaushik

---
+++ hibernate-basic/src/main/java/com/nish/kaushik/dto/User.java
@@ -0,0 +1,57 @@
+package com.nish.kaushik.dto;
+
+import javax.persistence.*;
+
+//@Table(name="USER")
+@Entity(name = "USER")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String phone;
+    @Embedded
+    private Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
