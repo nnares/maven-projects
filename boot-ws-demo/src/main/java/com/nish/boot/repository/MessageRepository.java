f8ce1fe9802b98f1f5a69fdd2f24abdac774fa9e
nnares <nishchay.naresh@gmail.com>
2021-01-30 10:54:47 +0530

    - boot-ws-demo

--- boot-ws-demo/src/main/java/com/nish/boot/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nish.boot.repository;
-
-import com.nish.boot.model.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<Message, Integer> {
-
-}
