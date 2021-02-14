229e5f21090458903c58ab9817eddb28a79ba6de
nnares <nishchay.naresh@gmail.com>
2021-02-15 00:39:17 +0530

    +u com.nish.springannotation.atvalue

--- spring-basic/src/main/java/com/nish/springannotation/atconfiguration/Country.java
+++ spring-basic/src/main/java/com/nish/springannotation/atconfiguration/Country.java
@@ -5,6 +5,17 @@ public class Country {
     private String name;
     private String ccy;
 
+    public Country() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCcy(String ccy) {
+        this.ccy = ccy;
+    }
+
     public Country(String name, String ccy) {
         this.name = name;
         this.ccy = ccy;
