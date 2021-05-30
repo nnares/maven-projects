2df24f66e9d11522656865bf3452f2d039a6f27d
nnares <nishchay.naresh@gmail.com>
2021-05-30 13:28:26 +0530

    +u JdbcRoute.java

--- camel-demo/src/main/java/com/nish/camel/route/JdbcRoute.java
+++ camel-demo/src/main/java/com/nish/camel/route/JdbcRoute.java
@@ -9,16 +9,12 @@ public class JdbcRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		final String sql = "select * from contact";
-		//from("sql:select * from emptab?dataSource=#dsObj")
+        //from("timer:first-time?period=2000")
 		from("timer://my-timer?repeatCount=2&period=4h")
+
 		.setBody(constant(sql))
 		.log("${body}")
 		.to("jdbc:dataSource")
-		.process(ex->{
-			Object ob=ex.getIn().getBody();
-			System.out.println(ob.toString());
-			ex.getMessage().setBody(ob.toString());
-		})
 		.to( "file:" + "{{destination.file.path}}" + "?fileName=contact_tbl.txt");
 	}
 }
