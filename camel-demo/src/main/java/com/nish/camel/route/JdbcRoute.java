82eca700eaf55005973b89b0eb366303a3841716
nnares <nishchay.naresh@gmail.com>
2021-06-07 10:56:15 +0530

    +u camel-demo

--- camel-demo/src/main/java/com/nish/camel/route/JdbcRoute.java
+++ camel-demo/src/main/java/com/nish/camel/route/JdbcRoute.java
@@ -2,9 +2,11 @@ package com.nish.camel.route;
 
 import com.nish.camel.bean1.Contact;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @Component
@@ -13,31 +15,30 @@ public class JdbcRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		final String sql = "select * from contact";
-        //from("timer:first-time?period=2000")
 
+		from("timer://my-timer?repeatCount=1")
+				.to("direct:fetchDBRoute")
+				.log("${body}")
+				.marshal().json(true)
+				.to("file:" + "{{destination.file.path}}" + "?fileName=contact.json");
+
+
+/*
 		from("timer://my-timer?repeatCount=2&period=4h")
 
 				.to("direct:fetchDBRoute")
 				.log("${body}")
+
 				.process(exchange -> {
 					// List to String
-/*
-					String body = exchange.getIn().getBody(String.class);
-					StringBuilder sb = new StringBuilder();
-					Arrays.stream(body.split("},"))
-							.forEach(s -> {
-								sb.append(s + "},\n");
-							});
-	*/
-					List<Contact> contacts = exchange.getIn().getBody(List.class);
+					List contacts = exchange.getIn().getBody(List.class);
 
 					StringBuilder sb = new StringBuilder();
 					contacts.forEach(e -> sb.append(e + ",\n"));
 					exchange.getIn().setBody(sb);
 				})
-
-				.to("file:" + "{{destination.file.path}}" + "?fileName=contact_tbl.txt");
-//				.to("file:" + "{{destination.file.path}}" + "?fileName=contact_tbl.txt?fileExist=Append");
+				.to("file:" + "{{destination.file.path}}" + "?fileName=contact_tbl.txt&fileExist=Append");
+*/
 
 		from("direct:fetchDBRoute")
 				.log("Get all contacts")
