4bb23e7295d672ab7d337bbacdfae4fabba0e110
nnares <nishchay.naresh@gmail.com>
2021-06-08 11:22:59 +0530

    +u camel-demo

---
+++ camel-demo/src/main/java/com/nish/camel/rest/CamelController.java
@@ -0,0 +1,31 @@
+package com.nish.camel.rest;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CamelController {
+
+    @Autowired
+    ProducerTemplate producerTemplate;
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public void startCamel() {
+        producerTemplate.sendBody("direct:firstRoute", "Calling via Spring Boot Rest Controller");
+    }
+
+    @RequestMapping(value = "/addContact", method = RequestMethod.GET)
+    public void addContact() {
+        producerTemplate.sendBody("direct:performDBInsert", "Calling via Spring Boot Rest Controller");
+    }
+
+    @RequestMapping(value = "/getAllContact", method = RequestMethod.GET)
+    public void getAllContact() {
+        producerTemplate.sendBody("direct:fetchDBRoute");
+    }
+
+}
