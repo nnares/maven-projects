3d192f424f6cdf5bfc8c32596d63e7d40db25dfd
nnares <nishchay.naresh@gmail.com>
2021-06-13 14:22:24 +0530

    +u camel-demo

---
+++ camel-demo/src/main/java/com/nish/camel/bean1/Contact.java
@@ -0,0 +1,62 @@
+package com.nish.camel.bean1;
+
+public class Contact {
+
+    private Integer contactId;
+    private String name;
+    private String email;
+    private String address;
+    private String phone;
+
+
+    public Integer getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(Integer contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "contactId=" + contactId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
