8e5644f0d63746f7aad060d8341e4a47eb1b9d04
nnares <nishchay.naresh@gmail.com>
2021-06-15 18:48:32 +0530

    +u camel-demo

--- camel-demo/src/main/java/com/nish/camel/route/JdbcRoute.java
+++ camel-demo/src/main/java/com/nish/camel/route/JdbcRoute.java
@@ -9,21 +9,22 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-@Component
+//@Component
 public class JdbcRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
 		final String sql = "select * from contact";
 
+/*
 		from("timer://my-timer?repeatCount=1")
 				.to("direct:fetchDBRoute")
 				.log("${body}")
 				.marshal().json(true)
 				.to("file:" + "{{destination.file.path}}" + "?fileName=contact.json");
+*/
 
 
-/*
 		from("timer://my-timer?repeatCount=2&period=4h")
 
 				.to("direct:fetchDBRoute")
@@ -38,12 +39,12 @@ public class JdbcRoute extends RouteBuilder {
 					exchange.getIn().setBody(sb);
 				})
 				.to("file:" + "{{destination.file.path}}" + "?fileName=contact_tbl.txt&fileExist=Append");
-*/
 
 		from("direct:fetchDBRoute")
 				.log("Get all contacts")
 				.setBody(constant(sql))
 				.to("jdbc:dataSource");
+
 
 	}
 }
