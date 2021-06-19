4d08f6a48476b5f87950b75baca412cbf87b0262
nnares <nishchay.naresh@gmail.com>
2021-06-19 19:33:42 +0530

    +u camel-demo

--- camel-demo/src/main/java/com/nish/camel/ftl/FtlRoute.java
+++ camel-demo/src/main/java/com/nish/camel/ftl/FtlRoute.java
@@ -4,6 +4,7 @@ import com.nish.camel.bean1.Contact;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,12 +19,12 @@ public class FtlRoute extends RouteBuilder {
 
 
 
-        from("direct:ftl-conversion")
+/*        from("direct:ftl-conversion")
                 .to("direct:fetchDBRoute")
                 .log("${body}")
 
                 .process(exchange -> {
-                    // List to String
+*//*                    // List to String
                     List contacts = exchange.getIn().getBody(List.class);
 
                     List<Contact> contacts1 = (List<Contact>) contacts;
@@ -35,9 +36,36 @@ public class FtlRoute extends RouteBuilder {
                         variableMap.put("telephone", curr.getPhone());
                         exchange.getIn().setHeader("CamelFreemarkerDataModel", variableMap);
                     }
+*//*
+
+                    List<HashMap<String,Object>> out = (ArrayList<HashMap<String,Object>>) exchange.getIn().getBody();
+
+                    try{
+                        for(HashMap<String,Object> currentMap : out){
+                            exchange.getIn().setHeader("CamelFreemarkerDataModel", currentMap);
+                            System.out.println(currentMap);
+                        }
+                    }catch(Exception e){
+                        System.out.println(e);
+                    }
 
                 })
                 .to("freemarker:templates/contact.ftl")
+                .log("${body}");*/
+
+        from("timer:first-time?period=2000")
+                .process(exchange -> {
+
+                    Map<String, Object> variableMap = new HashMap<>();
+
+                    variableMap.put("contact_id", 500);
+                    variableMap.put("name", "Alok Kumar");
+                    variableMap.put("email", "alok.kumar@example.com");
+                    variableMap.put("address", "Baadh, Bihar");
+                    variableMap.put("telephone", "9944994499");
+                    exchange.getIn().setHeader("CamelFreemarkerDataModel", variableMap);
+                })
+                .to("freemarker:templates/contact.ftl?contentCache=false")
                 .log("${body}");
 
     }
