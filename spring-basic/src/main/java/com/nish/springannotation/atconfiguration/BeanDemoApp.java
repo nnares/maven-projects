9c1f61daa402ead7a3397ff9b8da21361d0b3a3e
nnares <nishchay.naresh@gmail.com>
2021-09-04 00:08:19 +0530

    +u spring-basic

--- spring-basic/src/main/java/com/nish/springannotation/atconfiguration/BeanDemoApp.java
+++ spring-basic/src/main/java/com/nish/springannotation/atconfiguration/BeanDemoApp.java
@@ -12,13 +12,16 @@ public class BeanDemoApp {
         context.scan("com.nish.springannotation.atconfiguration");
         context.refresh();
 
+        Country us = context.getBean("getCountry", Country.class);
+        System.out.println("us = " + us);
+
         // extracting bean using bean name - countryObj
-        Country country = context.getBean("countryObj", Country.class);
-        System.out.println("country = " + country);
+        Country india = context.getBean("countryObj", Country.class);
+        System.out.println("india = " + india);
 
         // extracting bean using bean name - feedService
         // by default instances of this class have the same name as the methodName annotated with @Bean
-        TradeFeedService tradeFeedService = context.getBean("feedService", TradeFeedService.class);
+        TradeFeedService tradeFeedService = context.getBean("anyMethodName", TradeFeedService.class);
         tradeFeedService.loadTradeFeed();
 
         context.close();
