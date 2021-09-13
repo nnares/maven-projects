573ad43313b85d90ba1627f657dad34e1d12979c
nnares <nishchay.naresh@gmail.com>
2021-09-14 00:11:05 +0530

    +u com.nish.springannotation.atvalue

--- spring-basic/src/main/java/com/nish/springannotation/atvalue/AppConfig.java
+++ spring-basic/src/main/java/com/nish/springannotation/atvalue/AppConfig.java
@@ -2,10 +2,8 @@ package com.nish.springannotation.atvalue;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -21,28 +19,19 @@ import java.util.Map;
 @PropertySource(value = {"classpath:application.properties"})
 public class AppConfig {
 
-    /*
-     * PropertySourcesPlaceHolderConfigurer Bean only required for @Value("{}") annotations.
-     * Remove this bean if you are not using @Value annotations for injecting properties.
-     */
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
     @Bean // default name will be - monthMap , same as method name
     public Map<String, String> monthMap(@Value("${month.code.jan}") String jan,
-                                           @Value("${month.code.feb}") String feb,
-                                           @Value("${month.code.mar}") String mar,
-                                           @Value("${month.code.apr}") String apr,
-                                           @Value("${month.code.may}") String may,
-                                           @Value("${month.code.jun}") String jun,
-                                           @Value("${month.code.jul}") String jul,
-                                           @Value("${month.code.aug}") String aug,
-                                           @Value("${month.code.sep}") String sep,
-                                           @Value("${month.code.oct}") String oct,
-                                           @Value("${month.code.nov}") String nov,
-                                           @Value("${month.code.dec}") String dec) {
+                                        @Value("${month.code.feb}") String feb,
+                                        @Value("${month.code.mar}") String mar,
+                                        @Value("${month.code.apr}") String apr,
+                                        @Value("${month.code.may}") String may,
+                                        @Value("${month.code.jun}") String jun,
+                                        @Value("${month.code.jul}") String jul,
+                                        @Value("${month.code.aug}") String aug,
+                                        @Value("${month.code.sep}") String sep,
+                                        @Value("${month.code.oct}") String oct,
+                                        @Value("${month.code.nov}") String nov,
+                                        @Value("${month.code.dec}") String dec) {
 
         Map<String, String> monthMap = new LinkedHashMap<>();
         monthMap.put(jan, "January");
